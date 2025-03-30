5a12fea895516ea57de83ba049943379ebb3539c
Aditya0530 <adityaasonawane121@gmail.com>
2025-03-30 09:55:18 +0530

    Application Properties

--- User-ServiceDomain/src/main/java/com/ecommerce/main/dto/ErrorDto.java
+++ User-ServiceDomain/src/main/java/com/ecommerce/main/dto/ErrorDto.java
@@ -1,5 +1,8 @@
 package com.ecommerce.main.dto;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
@@ -10,6 +13,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class ErrorDto {
+
 	private String message;
-	private LocalDateTime timeDate;
+	private String date;
+	private String time;
+
+	public ErrorDto(String message) {
+		this.message = message;
+		this.date = new SimpleDateFormat("yyyy-MM-dd").format(new Date(System.currentTimeMillis()));
+		this.time = new SimpleDateFormat("HH:mm:ss").format(new Time(System.currentTimeMillis()));
+
+	}
 }
