84e3f0d4b89f8d7a4f8001f386f0b0dfc45b2fcb
Aditya0530 <adityaasonawane121@gmail.com>
2025-05-17 09:55:11 +0530

    Payment Api Added

--- User-ServiceDomain/src/main/java/com/ecommerce/main/controller/UserController.java
+++ User-ServiceDomain/src/main/java/com/ecommerce/main/controller/UserController.java
@@ -97,5 +97,5 @@ public class UserController {
 			@PathVariable("productId") int productId) {
 		userService.removeFromCart(userId, productId);
 		return new ResponseEntity<String>("Products Remove successfully", HttpStatus.OK);
-	      }
+	      }                                                           
 }
